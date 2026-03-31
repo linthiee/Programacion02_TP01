496b1de58ecc675b553e4e551150f3ddbe822022
elu <eluujazmin@gmail.com>
2026-03-31 13:07:22 -0300

    problem with directories

--- TP01_P2_MousseigneEluney/src/myListMousseigne.h
+++ TP01_P2_MousseigneEluney/src/myListMousseigne.h
@@ -10,12 +10,7 @@ namespace Mousseigne
 		T data;
 		Node* prev;
 		Node* next;
-		Node(const T& value)
-		{
-			this->data = value;
-			this->prev = nullptr;
-			this->next = nullptr;
-		}
+		Node(const T& value);
 	private:
 	};
 	template <typename N>
@@ -25,180 +20,27 @@ namespace Mousseigne
 		Node<N>* head;
 		Node<N>* tail;
 		int count;
-		Node<N>* getNodeAt(int index) const
-		{
-			if (index < 0 || index >= count)
-			{
-				throw std::out_of_range("Index out of range");
-			}
-			Node<N>* current;
-			if (index < count / 2)
-			{
-				current = head;
-				for (int i = 0; i < index; i++)
-				{
-					current = current->next;
-				}
-			}
-			else
-			{
-				current = tail;
-				for (int i = count - 1; i > index; i--)
-				{
-					current = current->prev;
-				}
-			}
-			return current;
-		}
+		Node<N>* getNodeAt(int index) const;
+
 	public:
-		MyList()
-		{
-			this->head = nullptr;
-			this->tail = nullptr;
-			this->count = 0;
-		}
-		~MyList() { clear(); }
-		void pushFront(const N& value)
-		{
-			Node<N>* newNode = new Node<N>(value);
-			if (head == nullptr)
-			{
-				tail = newNode;
-				head = tail;
-			}
-			else
-			{
-				newNode->next = head;
-				head->prev = newNode;
-				head = newNode;
-			}
-			count++;
-		}
-		void pushBack(const N& value)
-		{
-			Node<N>* newNode = new Node<N>(value);
-			if (tail == nullptr)
-			{
-				tail = newNode;
-				head = tail;
-			}
-			else
-			{
-				tail->next = newNode;
-				newNode->prev = tail;
-				tail = newNode;
-			}
-			count++;
-		}
-		N popFront()
-		{
-			if (head == nullptr)
-			{
-				throw std::out_of_range("List is empty");
-			}
-			N value = head->data;
-			Node<N>* temp = head;
-			head = head->next;
-			if (head != nullptr)
-			{
-				head->prev = nullptr;
-			}
-			else
-			{
-				tail = nullptr;
-			}
-			delete temp;
-			count--;
-			return value;
-		}
-		N popBack()
-		{
-			if (tail == nullptr)
-			{
-				throw std::out_of_range("List is empty");
-			}
-			N value = tail->data;
-			Node<N>* temp = tail;
-			tail = tail->prev;
-			if (tail != nullptr)
-			{
-				tail->next = nullptr;
-			}
-			else
-			{
-				head = nullptr;
-			}
-			delete temp;
-			count--;
-			return value;
-		}
-		void clear()
-		{
-			Node<N>* current = head;
-			while (current != nullptr)
-			{
-				Node<N>* next = current->next;
-				delete current;
-				current = next;
-			}
-			tail = nullptr;
-			head = nullptr;
-			count = 0;
-		}
-		int getCount() const { return count; }
-		void insertAt(int index, const N& value)
-		{
-			if (index < 0 || index > count)
-			{
-				throw std::out_of_range("Index out of range");
-			}
-			if (index == 0)
-			{
-				pushFront(value);
-			}
-			else if (index == count)
-			{
-				pushBack(value);
-			}
-			else
-			{
-				Node<N>* nextNode = getNodeAt(index);
-				Node<N>* prevNode = nextNode->prev;
-				Node<N>* newNode = new Node<N>(value);
-				prevNode->next = newNode;
-				newNode->prev = prevNode;
-				newNode->next = nextNode;
-				nextNode->prev = newNode;
-				count++;
-			}
-		}
-		void removeAt(int index)
-		{
-			if (index < 0 || index >= count)
-			{
-				throw std::out_of_range("Index out of range");
-			}
-			if (index == 0)
-			{
-				popFront();
-			}
-			else if (index == count - 1)
-			{
-				popBack();
-			}
-			else
-			{
-				Node<N>* nodeToRemove = getNodeAt(index);
-				Node<N>* prevNode = nodeToRemove->prev;
-				Node<N>* nextNode = nodeToRemove->next;
-				prevNode->next = nextNode;
-				nextNode->prev = prevNode;
-				delete nodeToRemove;
-				count--;
-			}
-		}
-		N& operator[](int index) { return getNodeAt(index)->data; }
-		const N& operator[](int index) const { return getNodeAt(index)->data; }
+		MyList();
+		~MyList();
+
+		void pushFront(const N& value);
+		void pushBack(const N& value);
+
+		N popFront();
+		N popBack();
+
+		void clear();
+
+		int getCount() const;
+
+		void insertAt(int index, const N& value);
+		void removeAt(int index);
+
+		N& operator[](int index);
+		const N& operator[](int index) const;
 		void print() const
 		{
 			Node<N>* current = head;
@@ -211,4 +53,5 @@ namespace Mousseigne
 			std::cout << "]\n";
 		}
 	};
+#include "myListMousseigne.tpp"
 };
--- TP01_P2_MousseigneEluney/src/myStackMousseigne.h
+++ TP01_P2_MousseigneEluney/src/myStackMousseigne.h
@@ -12,57 +12,22 @@ namespace Mousseigne
 		public:
 			T data;
 			Node* next;
-			Node(const T& value)
-			{
-				data = value;
-				next = nullptr;
-			}
+			Node(const T& value);
 		};
 		Node* topNode;
 		int count;
 	public:
-		MyStack()
-		{
-			topNode = nullptr;
-			count = 0;
-		}
-		~MyStack() { clear(); }
-		void push(const T& value)
-		{
-			Node* newNode = new Node(value);
-			newNode->next = topNode;
-			topNode = newNode;
-			count++;
-		}
-		T pop()
-		{
-			if (isEmpty())
-			{
-				throw std::runtime_error("The stack is empty.\n");
-			}
-			Node* tempNode = topNode;
-			T value = tempNode->data;
-			topNode = topNode->next;
-			count--;
-			delete tempNode;
-			return value;
-		}
-		T top()
-		{
-			if (isEmpty())
-			{
-				throw std::runtime_error("The stack is empty.\n");
-			}
-			return topNode->data;
-		}
-		bool isEmpty() const { return (count == 0); }
-		void clear()
-		{
-			while (!isEmpty())
-			{
-				pop();
-			}
-		}
-		int getCount() const { return count; }
+		MyStack();
+		~MyStack();
+
+		void push(const T& value);
+
+		T pop();
+		T top();
+
+		bool isEmpty() const;
+		void clear();
+
+		int getCount() const;
 	};
 }
